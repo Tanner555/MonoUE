b6699ede64230a3631bbbba1fc0a7e159a3f9e14
Tanner555 <theman989898@gmail.com>
2019-01-18 07:52:54 -0800

    Updated SubKey At Very Bottom, While Removing My Original Modified Code
    This is the suggestion made my pixeltris, which made more sense to begin with. The original key url made no sense at all.

--- Source/Programs/MonoUE.MSBuildSdkResolver/MonoUESdkResolver.cs
+++ Source/Programs/MonoUE.MSBuildSdkResolver/MonoUESdkResolver.cs
@@ -202,66 +202,24 @@ namespace MonoUE.MSBuildSdkResolver
 				}
 			}
 
-            string localSoftwareUEKeyString = @"SOFTWARE\EpicGames\Unreal Engine\";
-            using (var localkey64 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
-            using (var localSoftwareUEKey = localkey64.OpenSubKey(localSoftwareUEKeyString))
-            {
-                if(localSoftwareUEKey != null)
-                {
-                    string[] subkeyNames = localSoftwareUEKey.GetSubKeyNames();
-                    //Used to get the UE4 installation path from the Local Machine Registry
-                    string ue4MajorVersion = string.Empty;
-                    foreach (string subKeyName in subkeyNames)
-                    {
-                        //If SubKey Is Equal To Engine ID, Or If ID From GUID Isn't Empty And SubKey Is Equal To It
-                        if(subKeyName == engineID || (!string.IsNullOrEmpty(engineIDFromGUID) && subKeyName == engineIDFromGUID))
-                        {
-                            ue4MajorVersion = subKeyName;
-                            break;
-                        }
-                    }
-                    //If Major UE4 Version is Found, Then Retrieve InstalledDirectory Key Value
-                    if (!string.IsNullOrEmpty(ue4MajorVersion))
-                    {
-                        using (var localSoftwareUEMajorInstallKey = localkey64.OpenSubKey(localSoftwareUEKeyString + ue4MajorVersion))
-                        {
-                            string valueName = "InstalledDirectory";
-                            var path = (string)localSoftwareUEMajorInstallKey.GetValue(valueName);
-                            if (!string.IsNullOrEmpty(path) && Directory.Exists(path))
-                            {
-                                yield return new EngineInstallation { ID = ue4MajorVersion, Path = path };
-                            }
-                        }
-                    }
-                }
-            }
-
             using (var hkcu64 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
-            using (var key = hkcu64.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"))
+            using (var key = hkcu64.OpenSubKey(@"SOFTWARE\Epic Games\Unreal Engine"))
             {
                 foreach (string subkeyName in key.GetSubKeyNames())
                 {
                     using (RegistryKey subkey = key.OpenSubKey(subkeyName))
                     {
-                        string displayName = subkey.GetValue("DisplayName") as string;
-                        if (displayName == "Unreal Engine")
+                        string installDirectory = subkey.GetValue("InstalledDirectory") as string;
+                        if (!string.IsNullOrEmpty(installDirectory) && Directory.Exists(installDirectory))
                         {
-                            string installLocation = subkey.GetValue("InstallLocation") as string;
-                            if (!string.IsNullOrEmpty(installLocation) && Directory.Exists(installLocation))
+                            DirectoryInfo engineVersion = new DirectoryInfo(installDirectory);
+                            if (Directory.Exists(Path.Combine(engineVersion.FullName, "Engine")))
                             {
-                                DirectoryInfo directory = new DirectoryInfo(installLocation);
-                                foreach (DirectoryInfo engineVersion in directory.GetDirectories())
-                                {
-                                    if (Directory.Exists(Path.Combine(engineVersion.FullName, "Engine")) &&
-                                       Directory.Exists(Path.Combine(engineVersion.FullName, "Templates")))
-                                    {
-                                        yield return new EngineInstallation { ID = engineVersion.Name, Path = engineVersion.FullName };
+                                yield return new EngineInstallation { ID = engineVersion.Name, Path = engineVersion.FullName };
 
-                                        if (engineVersion.Name.StartsWith("UE_") && engineVersion.Name.Length > 3)
-                                        {
-                                            yield return new EngineInstallation { ID = engineVersion.Name.Substring(3), Path = engineVersion.FullName };
-                                        }
-                                    }
+                                if (engineVersion.Name.StartsWith("UE_") && engineVersion.Name.Length > 3)
+                                {
+                                    yield return new EngineInstallation { ID = engineVersion.Name.Substring(3), Path = engineVersion.FullName };
                                 }
                             }
                         }
